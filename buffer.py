3f0c1f9df60544741efadf76d3aa7163745a2782
Robert Cowher <bobcowher@gmail.com>
2025-04-27 21:20:42 -0500

    Buffers are now objects

--- buffer.py
+++ buffer.py
@@ -6,8 +6,8 @@ class ReplayBuffer():
     def __init__(self, max_size, input_shape, n_actions):
         self.mem_size = max_size
         self.mem_ctr = 0
-        self.state_memory = np.zeros((self.mem_size, *input_shape))
-        self.new_state_memory = np.zeros((self.mem_size, *input_shape))
+        self.state_memory = np.zeros(self.mem_size, object)
+        self.new_state_memory = np.zeros(self.mem_size, object)
         self.action_memory = np.zeros((self.mem_size, n_actions))
         self.reward_memory = np.zeros(self.mem_size)
         self.terminal_memory = np.zeros(self.mem_size, dtype=bool)
