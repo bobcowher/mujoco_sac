1be4e90db2ed62318bf55ecd0558ff062d49463b
Robert Cowher <bobcowher@gmail.com>
2025-05-01 20:40:10 -0500

    Updated train and stop training to be more successful

--- stop_training.sh
+++ stop_training.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 pgrep -f train | xargs kill -9
 pgrep -f tensorboard | xargs kill -9
+sleep 10
+pgrep -f tensorboard | xargs kill -9
+ps -ef | grep tensor
+ps -ef | grep train
--- train.sh
+++ train.sh
@@ -3,5 +3,5 @@ mkdir -p logs
 export MUJOCO_GL=egl
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate mujoco_sac
-nohup python ./train.py > logs/train.log 2>&1 & 
-nohup tensorboard --logdir runs --port 4444 --bind_all > logs/tensorboard.log 2>&1 &
+nohup python ./train.py >> logs/train.log 2>&1 & 
+nohup tensorboard --logdir runs --port 4444 --bind_all >> logs/tensorboard.log 2>&1 &
