a4f0cae8b39a9db2f9e1a39cbeeba90233583405
Robert Cowher <bobcowher@gmail.com>
2025-06-29 19:08:20 -0500

    Setting entropy_scalar to 0.06

--- train.py
+++ train.py
@@ -33,7 +33,7 @@ if __name__ == '__main__':
     max_episode_steps=2000 # max episode steps
     alpha_decay = 0.0002
     step_repeat = 1 
-    entropy_scalar = 0.1
+    entropy_scalar = 0.06
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
